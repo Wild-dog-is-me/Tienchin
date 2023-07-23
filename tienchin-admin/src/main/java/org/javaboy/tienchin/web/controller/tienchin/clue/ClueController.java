2a993312106647136375b4901ad500bdd2987f36
Wild-dog-is-me <1427774041@qq.com>
2023-07-23 16:37:27 +0800

    线索模块初始化

---
+++ tienchin-admin/src/main/java/org/javaboy/tienchin/web/controller/tienchin/clue/ClueController.java
@@ -0,0 +1,19 @@
+package org.javaboy.tienchin.web.controller.tienchin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Odin
+ * @since 2023-07-23
+ */
+
+@RestController
+@RequestMapping("/tienchin/clue")
+public class ClueController {
+
+}
